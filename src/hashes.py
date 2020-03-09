d7579e553944283bb546911ef48b808e9302901d
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2020-03-09 17:22:55 -0500

    insert retrieve and resize working passing tests

--- src/hashes.py
+++ src/hashes.py
@@ -6,7 +6,7 @@ key2 = 'string'.encode()
 print(key2)
 
 for i in range (n):
-    print(hashlib.sha256(key).hexdigest())
+    print(hashlib.sha256(key).hexdigest() % 2)
 
 for i in range (n):
     print(hash(key2))
